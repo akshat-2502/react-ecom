d277e954276c451c2a8c01d5385cfd6eaa54b278
akshat-2502 <khulbeakshat@gmail.com>
2025-05-16 20:21:29 +0530

    message for the contribution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 function App() {
-  return <div className="text-red-500">hello worlds git repo init git</div>;
+  return (
+    <div className="text-red-500">
+      hello worlds git repo init git
+      <div>
+        <h2>hello world</h2>
+      </div>
+    </div>
+  );
 }
 
 export default App;
